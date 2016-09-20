d58d84d287dfe80846c36fabfddf360f66a5460c
343748547 <343748547@C-DS23WR1.tdsb.on.ca>
2016-09-20 13:21:58 -0400

    Commiting about me.java

---
+++ AboutMe/src/Campos/AboutMe.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package Campos;
+
+/**
+ * @author 343748547
+ *
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Lucas C.");
+		System.out.println("Kemp");
+		System.out.println("Runnymede CI");
+		System.out.println("\"Go Reavens Go\"");
+		
+		System.out.format("%-10s %10s %18s","Weekdays\t","Class Code\t", "Class Start Times\n");
+		System.out.format("%-10s %10s %18s", "Monday-Friday\t", "AWS2O1-01\t", "08:45\n");
+		System.out.format("%-10s %10s %18s", "Monday-Friday\t", "Spare\t", "10:07\n");
+		System.out.format("%-10s %10s %18s", "Monday-Friday\t", "ICS3C-3U1-01\t", "12:26\n");
+		System.out.format("%-10s %10s %18s", "Monday-Friday\t", "ENG2P1-02\t", "13:48\n");
+	
+	}
+
+}
