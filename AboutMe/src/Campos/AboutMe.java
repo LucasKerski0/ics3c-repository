8fbbd1f068e9340a0295dd329f0941d4880aabba
343748547 <343748547@C-DS23WR1.tdsb.on.ca>
2016-09-20 13:30:19 -0400

    My first exercise.

--- AboutMe/src/Campos/AboutMe.java
+++ AboutMe/src/Campos/AboutMe.java
@@ -18,11 +18,11 @@ public class AboutMe {
 		System.out.println("Runnymede CI");
 		System.out.println("\"Go Reavens Go\"");
 		
-		System.out.format("%-10s %10s %18s","Weekdays\t","Class Code\t", "Class Start Times\n");
-		System.out.format("%-10s %10s %18s", "Monday-Friday\t", "AWS2O1-01\t", "08:45\n");
-		System.out.format("%-10s %10s %18s", "Monday-Friday\t", "Spare\t", "10:07\n");
-		System.out.format("%-10s %10s %18s", "Monday-Friday\t", "ICS3C-3U1-01\t", "12:26\n");
-		System.out.format("%-10s %10s %18s", "Monday-Friday\t", "ENG2P1-02\t", "13:48\n");
+		System.out.format("%-25s %15s %18s","\nWeekdays","Class Code\t", "Class Start Times\n");
+		System.out.format("%-25s %15s %18s", "Monday-Friday", "AWS2O1-01\t", "08:45\n");
+		System.out.format("%-25s %15s %18s", "Monday-Friday", "Spare\t", "10:07\n");
+		System.out.format("%-25s %15s %18s", "Monday-Friday", "ICS3C-3U1-01\t", "12:26\n");
+		System.out.format("%-25s %15s %18s", "Monday-Friday", "ENG2P1-02\t", "13:48");
 	
 	}
 
